aadf8345eec8995e917096f589b9eeaa1df8d95e
d.savuljesku <d.savuljesku@gmail.com>
2022-02-15 09:32:36 +0100

    Enable pre-shared key authentication

--- TuleapIntegration.setup.php
+++ TuleapIntegration.setup.php
@@ -5,15 +5,13 @@ require_once __DIR__ . '/TuleapIntegration.Dispatcher.php';
 if ( FARMER_IS_ROOT_WIKI_CALL ) {
 	wfLoadExtension( 'TuleapIntegration' );
 
-	// Load OAuth in main instance only
-	wfLoadExtension( 'OAuth' );
+	// Enable this code for OAuth authentication between Tuleap and wiki farm
+	// This will only load extension OAuth in the main instance
+	/*wfLoadExtension( 'OAuth' );
 	$wgGroupPermissions['sysop']['mwoauthproposeconsumer'] = true;
 	$wgGroupPermissions['sysop']['mwoauthupdateownconsumer'] = true;
 	$wgGroupPermissions['sysop']['mwoauthmanageconsumer'] = true;
 	$wgGroupPermissions['sysop']['mwoauthmanagemygrants'] = true;
 	$wgGroupPermissions['sysop']['mwoauthviewprivate'] = true;
-	$wgGroupPermissions['sysop']['mwoauthviewsuppressed'] = true;
-
-	$wgGrantPermissionGroups['farm-management'] = 'administration';
-	$wgGrantPermissions['farm-management']['tuleap-farm-manage'] = true;
+	$wgGroupPermissions['sysop']['mwoauthviewsuppressed'] = true;*/
 }
