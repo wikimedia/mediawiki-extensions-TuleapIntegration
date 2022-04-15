c1f1539d6c6d7e8f10cdb33fdef014b0c68a154b
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2022-04-15 12:25:13 +0000

    build: Updating composer dependencies
    * mediawiki/mediawiki-codesniffer: 29.0.0 → 38.0.0
      The following sniffs are failing and were disabled:
      * MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
    
    * mediawiki/minus-x: 1.0.0 → 1.1.1
    
    Additional changes:
    * Replaced "jakub-onderka" packages with "php-parallel-lint".
    * Dropped .php5 and .inc files from .phpcs.xml (T200956).
    * composer.json: Updated phpcs command in composer test (T280592).
    * composer.json: Added phpcs command to scripts (T280592).
    
    Change-Id: I6f7d7048e1bb5b3f89cc8b677d26a7f7935096b9

--- src/TuleapConnection.php
+++ src/TuleapConnection.php
@@ -194,7 +194,7 @@ class TuleapConnection {
 	 */
 	private function getNonce() {
 		return sodium_bin2base64(
-			random_bytes(32),
+			random_bytes( 32 ),
 			SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING
 		);
 	}
