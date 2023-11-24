2e08b7367abcc552becd4f0286d46db70913d5eb
rvogel <vogel@hallowelt.biz>
2023-11-24 09:10:43 +0100

    Add logging
    Change-Id: Ie177abcc5fa1bf90c3b07ac765b712b5cc99f072

--- src/UserMappingProvider.php
+++ src/UserMappingProvider.php
@@ -3,6 +3,7 @@
 namespace TuleapIntegration;
 
 use MediaWiki\User\UserFactory;
+use Psr\Log\LoggerInterface;
 use User;
 use Wikimedia\Rdbms\ILoadBalancer;
 
@@ -11,14 +12,17 @@ class UserMappingProvider {
 	private $lb;
 	/** @var UserFactory */
 	private $userFactory;
+	/** @var LoggerInterface */
+	private $logger;
 
 	/**	 *
 	 * @param ILoadBalancer $loadBalancer
 	 * @param UserFactory $userFactory
 	 */
-	public function __construct( ILoadBalancer $loadBalancer, UserFactory $userFactory ) {
+	public function __construct( ILoadBalancer $loadBalancer, UserFactory $userFactory, LoggerInterface $logger ) {
 		$this->lb = $loadBalancer;
 		$this->userFactory = $userFactory;
+		$this->logger = $logger;
 	}
 
 	/**
@@ -29,6 +33,7 @@ class UserMappingProvider {
 	public function provideUserForId( int $id ): ?string {
 		$db = $this->lb->getConnection( DB_REPLICA );
 		if ( !$db->tableExists( 'tuleap_user_mapping' ) ) {
+			$this->logger->info( 'Table `tuleap_user_mapping` does not exist' );
 			return null;
 		}
 		$row = $this->lb->getConnection( DB_REPLICA )->selectRow(
@@ -42,6 +47,7 @@ class UserMappingProvider {
 		);
 
 		if ( !$row ) {
+			$this->logger->debug( "Could not find local username mapping for id '$id'" );
 			return null;
 		}
 
@@ -51,6 +57,7 @@ class UserMappingProvider {
 			return $user;
 		}
 
+		$this->logger->debug( "Could not create valid user from name '$uname' (ID:$id)" );
 		return null;
 	}
 }
