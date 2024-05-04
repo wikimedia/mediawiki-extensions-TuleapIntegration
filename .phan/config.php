d26701f4f8976b3d8a403f23f0075718eaf7a759
Zoranzoki21 <zorandori4444@gmail.com>
2024-05-04 01:22:15 +0000

    Revert "Add local `vendor/` to PHAN config"
    This reverts commit d606a3028b5491ec88a0aa59711d6020fde802d0.
    
    Reason for revert: Breaks CI
    
    Change-Id: If9108c7637d1b5c7f509154fd084faf865a28e76

--- .phan/config.php
+++ .phan/config.php
@@ -1,16 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'],
-	[
-		__DIR__ . '../vendor'
-	]
-);
-$cfg['exclude_analysis_directory_list'] = array_merge(
-	$cfg['exclude_analysis_directory_list'],
-	[
-		__DIR__ . '../vendor'
-	]
-);
-return $cfg;
+return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
